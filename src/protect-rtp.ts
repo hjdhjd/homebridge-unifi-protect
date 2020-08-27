05f936fb6efd2bf38e22cb7664a78d7166cf3352
HJD <hjdhjd@users.noreply.github.com>
2020-08-27 14:47:38 -0500

    Add a utility to ensure we can reserve consecutive ports for ffmpeg + housekeeping.

--- src/protect-rtp.ts
+++ src/protect-rtp.ts
@@ -6,14 +6,15 @@
  * borrows heavily from both. Thank you for your contributions to the HomeKit world.
  */
 import { createSocket } from "dgram";
+import getPort from "get-port";
 import { Logging } from "homebridge";
 import { ProtectStreamingDelegate } from "./protect-stream";
+import { PROTECT_TWOWAY_HEARTBEAT_INTERVAL } from "./settings";
 
 // What this function does is create two socket pipes to split traffic coming in from serverPort and
-// pipe it out two audioRTCPPort and returnAudioPort.
+// pipe it out to returnAudioPort and twowayAudioPort.
 //
-// In order to support two-way audio, we send a complete copy of the inbound traffic from serverPort to
-// returnAudioPort.
+// In order to support two-way audio, we send out the inbound RTP traffic from serverPort to twowayAudioPort.
 //
 // For the audio channel that supports the video stream we're sending to the user, we only want to send the RTCP
 // information, because that channel is receiving all it's data (audio and video) from the video stream. RTCP
@@ -29,17 +30,17 @@ export class RtpSplitter {
   private heartbeatMsg!: Buffer;
   private log: Logging;
   private name: string;
-  private inboundPort: number;
+  private serverPort: number;
   public readonly socket;
 
   // Create an instance of RTPSplitter.
-  constructor(streamingDelegate: ProtectStreamingDelegate, ipFamily: ("ipv4" | "ipv6") , serverPort: number, audioRTCPPort: number, returnAudioPort: number) {
+  constructor(streamingDelegate: ProtectStreamingDelegate, ipFamily: ("ipv4" | "ipv6") , serverPort: number, returnAudioPort: number, twowayAudioPort: number) {
 
     this.debug = streamingDelegate.debug;
     this.delegate = streamingDelegate;
-    this.inboundPort = serverPort;
     this.log = streamingDelegate.log;
     this.name = streamingDelegate.name;
+    this.serverPort = serverPort;
     this.socket = createSocket(ipFamily === "ipv6" ? "udp6" : "udp4" );
 
     // Catch errors when they happen on our splitter.
@@ -51,32 +52,32 @@ export class RtpSplitter {
     // Split the message into RTP and RTCP packets.
     this.socket.on("message", (msg) => {
 
-      // Always send RTP and RTCP packets to both sides of the splitter.
-      this.socket.send(msg, returnAudioPort, "127.0.0.1");
+      // Send RTP packets to the return audio port.
+      if(this.isRtpMessage(msg)) {
+        this.socket.send(msg, twowayAudioPort, "127.0.0.1");
+      } else {
 
-      // RTCP control packets should go to the RTCP port.
-      if(!this.isRtpMessage(msg)) {
-
-        // Save this RTCP message for heartbeat purposes.
+        // Save this RTCP message for heartbeat purposes for the return audio port.
         this.heartbeatMsg = Buffer.from(msg);
 
         // Clear the old heartbeat timer.
         clearTimeout(this.heartbeatTimer);
-        this.heartbeat(returnAudioPort);
+        this.heartbeat(twowayAudioPort);
 
-        this.socket.send(msg, audioRTCPPort, "127.0.0.1");
+        // RTCP control packets should go to the RTCP port.
+        this.socket.send(msg, returnAudioPort, "127.0.0.1");
       }
     });
 
-    this.debug("%s: Creating an RtpSplitter instance - inbound port: %s, return audio port: %s, audio RTCP port: %s.",
-      this.name, this.inboundPort, returnAudioPort, audioRTCPPort);
+    this.debug("%s: Creating an RtpSplitter instance - inbound port: %s, twoway audio port: %s, return audio port: %s.",
+      this.name, this.serverPort, twowayAudioPort, returnAudioPort);
 
     // Take the socket live.
-    this.socket.bind(this.inboundPort);
+    this.socket.bind(this.serverPort);
   }
 
   // Send a regular heartbeat to FFmpeg to ensure the pipe remains open and the process alive.
-  private heartbeat(returnAudioPort: number): void {
+  private heartbeat(port: number): void {
 
     // Clear the old heartbeat timer.
     clearTimeout(this.heartbeatTimer);
@@ -88,14 +89,14 @@ export class RtpSplitter {
     this.heartbeatTimer = setTimeout(() => {
       this.debug("Sending ffmpeg a heartbeat.");
 
-      self.socket.send(self.heartbeatMsg, returnAudioPort, "127.0.0.1");
-      self.heartbeat(returnAudioPort);
-    }, 3.5 * 1000);
+      self.socket.send(self.heartbeatMsg, port, "127.0.0.1");
+      self.heartbeat(port);
+    }, PROTECT_TWOWAY_HEARTBEAT_INTERVAL * 1000);
   }
 
   // Close the socket and cleanup.
   close(): void {
-    this.debug("%s: Closing the RtpSplitter instance on port %s.", this.name, this.inboundPort);
+    this.debug("%s: Closing the RtpSplitter instance on port %s.", this.name, this.serverPort);
 
     clearTimeout(this.heartbeatTimer);
     this.socket.close();
@@ -111,5 +112,35 @@ export class RtpSplitter {
     const payloadType = this.getPayloadType(message);
 
     return (payloadType > 90) || (payloadType === 0);
+  }
+}
+
+// RTP-related utilities.
+export class RtpUtils {
+
+  // Reserve consecutive ports for use with FFmpeg. FFmpeg currently lacks the ability to specify both the RTP
+  // and RTCP ports. It always assumes, by convention, that when you specify an RTP port, the RTCP port is the
+  // RTP port + 1. In order to work around that challenge, we need to always ensure that when we reserve multiple
+  // ports for RTP (primarily for two-way audio) that we we are reserving consecutive ports only.
+  public static async reservePorts(count = 1): Promise<number[]> {
+
+    // Get the first port.
+    const port = await getPort();
+    const ports = [port];
+
+    // If we're requesting additional consecutive ports, keep searching until they're found.
+    for(let i = 1; i < count; i++) {
+      const targetConsecutivePort = port + i;
+      const openPort = await getPort({ port: targetConsecutivePort });
+
+      // Unable to reserve the next consecutive port. Roll the dice again and hope for the best.
+      if(openPort !== targetConsecutivePort) {
+        return this.reservePorts(count);
+      }
+
+      ports.push(openPort);
+    }
+
+    return ports;
   }
 }
