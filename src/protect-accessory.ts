2907866a2aa923ab8de675eec6379f3c1a9b3fde
HJD <hjdhjd@users.noreply.github.com>
2020-09-03 23:13:12 -0500

    Refactored to provide additional flexibility and reusability.

--- src/protect-accessory.ts
+++ src/protect-accessory.ts
@@ -8,6 +8,7 @@ import {
   Logging,
   PlatformAccessory
 } from "homebridge";
+import { ProtectApi } from "./protect-api";
 import { ProtectNvr } from "./protect-nvr";
 import { ProtectPlatform } from "./protect-platform";
 
@@ -16,27 +17,47 @@ export interface ProtectAccessory {
   configureDoorbellLcdSwitch?(): boolean;
 }
 
-export abstract class ProtectAccessory {
-  public readonly accessory: PlatformAccessory;
+export abstract class ProtectBase {
   public readonly api: API;
   public debug: (message: string, ...parameters: unknown[]) => void;
   protected readonly debugMode: boolean;
   protected readonly hap: HAP;
   protected readonly log: Logging;
   public readonly nvr: ProtectNvr;
+  public nvrApi: ProtectApi;
   public readonly platform: ProtectPlatform;
 
   // The constructor initializes key variables and calls configureDevice().
-  constructor(nvr: ProtectNvr, accessory: PlatformAccessory) {
-    this.accessory = accessory;
+  constructor(nvr: ProtectNvr) {
     this.api = nvr.platform.api;
     this.debug = nvr.platform.debug.bind(this);
     this.debugMode = nvr.platform.debugMode;
     this.hap = this.api.hap;
     this.log = nvr.platform.log;
     this.nvr = nvr;
+    this.nvrApi = nvr.nvrApi;
     this.platform = nvr.platform;
+  }
 
+  // Utility function to return the fully enumerated name of this camera.
+  public name(): string {
+    return this.nvr.nvrApi.getNvrName();
+  }
+}
+
+export abstract class ProtectAccessory extends ProtectBase {
+  public readonly accessory: PlatformAccessory;
+
+  // The constructor initializes key variables and calls configureDevice().
+  constructor(nvr: ProtectNvr, accessory: PlatformAccessory) {
+
+    // Call the constructor of our base class.
+    super(nvr);
+
+    // Set the accessory.
+    this.accessory = accessory;
+
+    // Configure the device.
     void this.configureDevice();
   }
 
