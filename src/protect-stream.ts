b368df0ab4702736fc1449005ed62e3565cbe1fe
HJD <hjdhjd@users.noreply.github.com>
2020-11-26 01:34:40 -0600

    New feature option: Video.Transcode.

--- src/protect-stream.ts
+++ src/protect-stream.ts
@@ -52,7 +52,7 @@ type SessionInfo = {
   videoSRTP: Buffer; // Key and salt concatenated.
   videoSSRC: number; // RTP synchronisation source.
 
-  hasLibFdk: boolean; // Does the user have a version of ffmpeg that supports AAC?
+  hasLibFdk: boolean; // Does the user have a version of FFmpeg that supports AAC?
   audioPort: number;
   audioIncomingRtcpPort: number;
   audioIncomingRtpPort: number; // Port to receive audio from the HomeKit microphone.
@@ -246,6 +246,7 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
       return;
     }
 
+    // Find the best RTSP stream based on what we're looking for.
     const rtspEntry = this.protectCamera.findRtsp(request.video.width, request.video.height, cameraConfig, sessionInfo.address);
 
     if(!rtspEntry) {
@@ -259,12 +260,15 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
       return;
     }
 
+    // Are we transcoding?
+    const isTranscoding = this.protectCamera.nvr.optionEnabled(cameraConfig, "Video.Transcode", false, sessionInfo.address);
+
     // Set our packet size to be 564. Why? MPEG transport stream (TS) packets are 188 bytes in size each.
     // These packets transmit the video data that you ultimately see on your screen and are transmitted using
     // UDP. Each UDP packet is 1316 bytes in size, before being encapsulated in IP. We want to get as many
     // TS packets as we can, within reason, in those UDP packets. This translates to 1316 / 188 = 7 TS packets
     // as a limit of what can be pushed through a single UDP packet. Here's the problem...you need to have
-    // enough data to fill that pipe, all the time. Network latency, ffmpeg overhead, and the speed / quality of
+    // enough data to fill that pipe, all the time. Network latency, FFmpeg overhead, and the speed / quality of
     // the original camera stream all play a role here, and as you can imagine, there's a nearly endless set of
     // combinations to decide how to best fill that pipe. Set it too low, and you're incurring extra overhead by
     // pushing less video data to clients in each packet, though you're increasing interactivity by getting
@@ -283,40 +287,74 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
 
     // -hide_banner:        suppress printing the startup banner in FFmpeg.
     // -rtsp_transport tcp: tell the RTSP stream handler that we're looking for a TCP connection.
-    const ffmpegArgs = [ "-hide_banner", "-rtsp_transport", "tcp", "-i", rtspEntry.url ];
+    // -map 0:v             selects the first available video track from the stream. Protect actually maps audio
+    //                      and video tracks in opposite locations from where FFmpeg typically expects them. This
+    //                      setting is a more general solution than naming the track locations directly in case
+    //                      Protect changes this in n the future.
+    const ffmpegArgs = [ "-hide_banner", "-rtsp_transport", "tcp", "-i", rtspEntry.url, "-map", "0:v" ];
 
-    this.log.info("%s: HomeKit video stream request received from %s: %sx%s@%sfps, %s kbps. Using the %s RTSP stream profile.",
-      this.name(), sessionInfo.address , request.video.width, request.video.height, request.video.fps, request.video.max_bit_rate, rtspEntry.name);
+    // Inform the user.
+    this.log.info("%s: Streaming request from %s: %sx%s@%sfps, %s kbps. %s RTSP stream profile: %s, %s kbps.",
+      this.name(), sessionInfo.address, request.video.width, request.video.height, request.video.fps, request.video.max_bit_rate,
+      isTranscoding ? "Transcoding" : "Using", rtspEntry.name, rtspEntry.channel.bitrate / 1000);
 
-    // Configure our video parameters:
-    // -map 0:v           selects the first available video track from the stream. Protect actually maps audio
-    //                    and video tracks in opposite locations from where ffmpeg typically expects them. This
-    //                    setting is a more general solution than naming the track locations directly in case
-    //                    Protect changes this in n the future.
-    // -vcodec copy       copy the stream withour reencoding it.
-    // -f rawvideo        specify that we're using raw video.
-    // -pix_fmt yuvj420p  use the yuvj420p pixel format, which is what Protect uses.
-    // -r fps             frame rate to use for this stream. This is specified by HomeKit.
-    // -b:v bitrate       the average bitrate to use for this stream. This is specified by HomeKit.
-    // -bufsize size      this is the decoder buffer size, which drives the variability / quality of the output bitrate.
-    // -maxrate bitrate   the maximum bitrate tolerance, used with -bufsize. We set this to max_bit_rate to effectively
-    //                    create a constant bitrate.
+    // Check to see if we're transcoding. If we are, set the right FFmpeg encoder options. If not, copy the video stream.
+    if(isTranscoding) {
+
+      // Configure our video parameters for transcoding:
+      //
+      // -vcodec libx264    copy the stream withour reencoding it.
+      // -pix_fmt yuvj420p  use the yuvj420p pixel format, which is what Protect uses.
+      // -profile:v high    use the H.264 high profile when encoding, which provides for better stream quality and size efficiency.
+      // -preset veryfast   use the veryfast encoding preset in libx264, which provides a good balance of encoding speed and quality.
+      // -bf 0              disable B-frames when encoding to increase compatibility against occasionally finicky HomeKit clients.
+      // -b:v bitrate       the average bitrate to use for this stream. This is specified by HomeKit.
+      // -bufsize size      this is the decoder buffer size, which drives the variability / quality of the output bitrate.
+      // -maxrate bitrate   the maximum bitrate tolerance, used with -bufsize. We set this to max_bit_rate to effectively
+      //                    create a constant bitrate.
+      // -filter:v fps=fps= use the fps filter to get to the frame rate requested by HomeKit. This has better performance characteristics
+      //                    for Protect rather than using "-r".
+      ffmpegArgs.push(
+        "-vcodec", "libx264",
+        "-pix_fmt", "yuvj420p",
+        "-profile:v", "high",
+        "-preset", "veryfast",
+        "-bf", "0",
+        "-b:v", request.video.max_bit_rate.toString() + "k",
+        "-bufsize", (2 * request.video.max_bit_rate).toString() + "k",
+        "-maxrate", request.video.max_bit_rate.toString() + "k",
+        "-filter:v", "fps=fps=" + request.video.fps.toString()
+      );
+
+    } else {
+
+      // Configure our video parameters for just copying the input stream from Protect - it tends to be quite solid in most cases:
+      //
+      // -vcodec copy       copy the stream withour reencoding it.
+      // -pix_fmt yuvj420p  use the yuvj420p pixel format, which is what Protect uses.
+      // -r fps             frame rate to use for this stream. This is specified by HomeKit.
+      // -b:v bitrate       the average bitrate to use for this stream. This is specified by HomeKit.
+      // -bufsize size      this is the decoder buffer size, which drives the variability / quality of the output bitrate.
+      // -maxrate bitrate   the maximum bitrate tolerance, used with -bufsize. We set this to max_bit_rate to effectively
+      //                    create a constant bitrate.
+      ffmpegArgs.push(
+        "-vcodec", "copy"
+      );
+    }
+
+    // Add in any user-specified options for FFmpeg.
+    if(this.platform.config.ffmpegOptions) {
+      ffmpegArgs.push(...this.platform.config.ffmpegOptions);
+    }
+
+    // Configure our video parameters for SRTP streaming:
+    //
     // -payload_type num  payload type for the RTP stream. This is negotiated by HomeKit and is usually 99 for H.264 video.
     // -ssrc                   synchronization source stream identifier. Random number negotiated by HomeKit to identify this stream.
     // -f rtp                  specify that we're using the RTP protocol.
     // -srtp_out_suite enc     specify the output encryption encoding suites.
     // -srtp_out_params params specify the output encoding parameters. This is negotiated by HomeKit.
     ffmpegArgs.push(
-      "-map", "0:v",
-      "-vcodec", "copy",
-      "-f", "rawvideo",
-      "-pix_fmt", "yuvj420p",
-      "-r", request.video.fps.toString(),
-      // "-force_key_frames", "expr:gte(t,n_forced*2)",
-      ...this.platform.config.ffmpegOptions.split(" "),
-      "-b:v", request.video.max_bit_rate.toString() + "k",
-      "-bufsize", (2 * request.video.max_bit_rate).toString() + "k",
-      "-maxrate", request.video.max_bit_rate.toString() + "k",
       "-payload_type", request.video.pt.toString(),
       "-ssrc", sessionInfo.videoSSRC.toString(),
       "-f", "rtp",
@@ -329,7 +367,7 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
     // Configure the audio portion of the command line, if we have a version of FFmpeg supports libfdk_aac. Options we use are:
     //
     // -map 0:a              selects the first available audio track from the stream. Protect actually maps audio
-    //                       and video tracks in opposite locations from where ffmpeg typically expects them. This
+    //                       and video tracks in opposite locations from where FFmpeg typically expects them. This
     //                       setting is a more general solution than naming the track locations directly in case
     //                       Protect changes this in the future.
     // -acodec libfdk_aac    encode to AAC.
@@ -448,7 +486,7 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
 
     // Configure the audio portion of the command line, if we have a version of FFmpeg supports libfdk_aac. Options we use are:
     //
-    // -protocol_whitelist   set the list of allowed protocols for this ffmpeg session.
+    // -protocol_whitelist   set the list of allowed protocols for this FFmpeg session.
     // -f sdp                specify that our input will be an SDP file.
     // -acodec libfdk_aac    decode AAC input.
     // -i pipe:0             read input from standard input.
@@ -464,12 +502,12 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
       "-protocol_whitelist", "pipe,udp,rtp,file,crypto",
       "-f", "sdp",
       "-acodec", "libfdk_aac",
+      "-ac", cameraConfig.talkbackSettings.channels.toString(),
       "-i", "pipe:0",
       "-acodec", cameraConfig.talkbackSettings.typeFmt,
       "-flags", "+global_header",
       "-ar", cameraConfig.talkbackSettings.samplingRate.toString(),
       "-b:a", "64k",
-      "-ac", cameraConfig.talkbackSettings.channels.toString(),
       "-f", "adts",
       "udp://" + cameraConfig.host + ":" + cameraConfig.talkbackSettings.bindPort.toString()
     ];
@@ -488,7 +526,7 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
     // Housekeeping for the twoway FFmpeg session.
     this.ongoingSessions[request.sessionID].ffmpeg.push(ffmpegReturnAudio);
 
-    // Feed the SDP session description to ffmpeg on stdin.
+    // Feed the SDP session description to FFmpeg on stdin.
     ffmpegReturnAudio.getStdin()?.write(sdpReturnAudio);
     ffmpegReturnAudio.getStdin()?.end();
   }
@@ -502,7 +540,7 @@ export class ProtectStreamingDelegate implements CameraStreamingDelegate {
         break;
 
       case StreamRequestTypes.RECONFIGURE:
-        // Once ffmpeg is updated to support this, we'll enable this one.
+        // Once FFmpeg is updated to support this, we'll enable this one.
         this.debug("%s: Ignoring request to reconfigure: %sx%s, %s fps, %s kbps.",
           this.name(), request.video.width, request.video.height, request.video.fps, request.video.max_bit_rate);
         callback();
