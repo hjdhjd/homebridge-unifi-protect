8055cf3405d09c0b44ae4fe8fc9411982b9318b7
HJD <hjdhjd@users.noreply.github.com>
2022-02-14 12:57:48 -0600

    Housekeeping.

--- src/protect-nvr-events.ts
+++ src/protect-nvr-events.ts
@@ -574,7 +574,8 @@ export class ProtectNvrEvents {
 
       // Log the event, if configured to do so.
       if(this.nvr.optionEnabled(device, "Log.Motion", false)) {
-        this.log.info("%s: Motion detected%s.", this.nvrApi.getFullName(device), detectedObjects.length ? ": " + detectedObjects.join(", ") : "");
+        this.log.info("%s: Motion detected%s.",
+          this.nvrApi.getFullName(device), (!protectCamera.stream.hksv?.isRecording && detectedObjects.length) ? ": " + detectedObjects.join(", ") : "");
       }
     }
 
