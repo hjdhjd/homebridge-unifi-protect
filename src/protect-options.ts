c26269fc080e05c17ea5884e2074eabc4ec37bfd
HJD <hjdhjd@users.noreply.github.com>
2024-10-14 21:33:07 -0500

    Change: default to hardware transcoding when available.

--- src/protect-options.ts
+++ src/protect-options.ts
@@ -166,7 +166,7 @@ export const featureOptions: { [index: string]: ProtectFeatureOption[] } = {
   // Video options.
   "Video": [
 
-    { default: false, description: "Use hardware-accelerated transcoding when available (Apple Macs, Intel Quick Sync Video-enabled CPUs, Raspberry Pi 4).", name: "Transcode.Hardware" },
+    { default: true, description: "Use hardware-accelerated transcoding when available (Apple Macs, Intel Quick Sync Video-enabled CPUs, Raspberry Pi 4).", name: "Transcode.Hardware" },
     { default: true, description: "Use the native Protect livestream API to view livestreams.", isNotProperty: [ "isThirdPartyCamera" ], name: "Stream.UseApi" },
     { default: true, description: "When streaming to low-latency clients (e.g. at home), transcode livestreams, instead of transmuxing them.", name: "Transcode" },
     { default: false, defaultValue: PROTECT_TRANSCODE_BITRATE, description: "Bitrate, in kilobits per second, to use when transcoding to low-latency (e.g. at home) clients, ignoring the bitrate HomeKit requests. HomeKit typically requests lower video quality than you may desire in your environment.", group: "Transcode", name: "Transcode.Bitrate" },
